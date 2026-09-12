c78942e940155dbbf8937e8da8e22472f64a6e0b
denor1999 <ozhgixinv@list.ru>
2026-09-12 09:57:08 +0300

    added simple status tests

---
+++ src/test/java/tests/StatusTests.java
@@ -0,0 +1,111 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.is;
+
+public class StatusTests {
+
+    @Test
+    public void totalAmountTest() {
+        get("https://selenoid.qa.guru/status")
+                .then()
+                .body("total", is(25));
+    }
+
+    @Test
+    public void totalAmountTestWithResponseLogs() {
+        get("https://selenoid.qa.guru/status")
+                .then()
+                .log().all()
+                .body("total", is(25));
+    }
+
+    @Test
+    public void totalAmountTestWithAllLogs() {
+        given()
+                .log().all()
+                .when()
+                .get("https://selenoid.qa.guru/status")
+                .then()
+                .log().all()
+                .body("total", is(25));
+    }
+
+    @Test
+    public void statusTest() {
+        given()
+                .log().uri()
+                .log().method()
+                .log().headers()
+                .when()
+                .get("https://selenoid.qa.guru/status")
+                .then()
+                .log().all()
+                .statusCode(200);
+    }
+
+    @Test
+    public void browserVersionsTest() {
+        given()
+                .log().uri()
+                .log().method()
+                .log().headers()
+                .when()
+                .get("https://selenoid.qa.guru/status")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("browsers.chrome", hasKey("151.0"))
+                .body("browsers.chrome", hasKey("152.0"));
+    }
+
+    @Test
+    public void requiredKeysTest() {
+        given()
+                .log().uri()
+                .log().method()
+                .log().headers()
+                .when()
+                .get("https://selenoid.qa.guru/status")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("", hasKey("total"))
+                .body("", hasKey("used"))
+                .body("", hasKey("queued"))
+                .body("", hasKey("pending"))
+                .body("", hasKey("browsers"));
+    }
+
+    @Test
+    public void statusSchemaTest() {
+        given()
+                .log().uri()
+                .log().method()
+                .log().headers()
+                .when()
+                .get("https://selenoid.qa.guru/status")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("schemas/status_response_schema.json"));
+    }
+
+    @Test
+    public void bestTotalAmountTest() {
+        given()
+                .log().all()
+                .when()
+                .get("https://selenoid.qa.guru/status")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("schemas/status_response_schema.json"))
+                .body("total", is(25));
+    }
+}
